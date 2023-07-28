7ab3bf25b429b718e872ab6fe928bb40560dae8f
etomaro <python.helloworld111@gmail.com>
2023-07-28 15:28:43 +0900

    引っ返せれる石を考慮

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ def get_next_state(state, action, player_id):
     # 周りの8隅の座標
     frame_places = [[row-1, col-1], [row-1, col], [row-1, col+1], [row, col-1], [row, col+1], [row+1, col-1], [row+1, col], [row+1, col+1]]
     # 周りの8個に相手の石があるかどうか。ある場合挟めるかどうか判定
+    change_places_all = []  # 挟んだすべての石の座標
     for frame_place in frame_places:
         frame_row, frame_col = frame_place
         # ボードの外に出る場合は除外
@@ -116,6 +117,7 @@ def get_next_state(state, action, player_id):
         if state[frame_row][frame_col] == OPPONENT_ID:
             # 挟める石を追加
             change_places.append([frame_row, frame_col])
+            change_places_all.append([frame_row, frame_col])
             # ベクトル(周りの8個の座標 - 相手の石の場所の座標)
             vector = [frame_row - row, frame_col - col]
             # 自分の石があるまでループ
@@ -142,6 +144,7 @@ def get_next_state(state, action, player_id):
                 if state[next_row][next_col] == OPPONENT_ID:
                     # 挟める石を追加
                     change_places.append([next_row, next_col])
+                    change_places_all.append([next_row, next_col])
 
                     next_row += vector[0]
                     next_col += vector[1]
@@ -153,7 +156,7 @@ def get_next_state(state, action, player_id):
 
     next_player_id = "0" if player_id == "1" else "1"
 
-    return next_state, next_player_id
+    return next_state, next_player_id, change_places_all
 
 
 # アクションを選択する
@@ -170,8 +173,10 @@ def choice(actionable, state, player_id):
           (現在の相手が取れる角の数 - 次の相手が取れる角の数) * 1万点 +
           (現在の相手が取れる端の数 - 次の相手が取れる端の数) * 100点 +
           (100点 - 次の相手の手数)
-          自分が角を取れるアクション + 1満点
-          自分が端を取れるアクション + 100点
+          自分が角を取れるアクション: 1万点
+          自分が端を取れるアクション: 100点
+          自分のアクションによってひっくり返した相手の端の数 * (200点) 
+          (現在の相手がひっくり返すことができる端の数 - 次の相手がひっくり返すことができる端の数) * 200点
 
 
 
@@ -186,6 +191,8 @@ def choice(actionable, state, player_id):
     opponent_corner_count = 0
     opponent_edge_count = 0
     now_opponet_actionables = get_actionables(state, OPPONENT_ID)
+    # 現在相手がひっくり返すことのできる端の数
+    now_reverse_my_edge_count = 0
     for action in now_opponet_actionables:
         # 角の場合
         if action == "a1" or action == "a8" or action == "h1" or action == "h8":
@@ -195,12 +202,49 @@ def choice(actionable, state, player_id):
              action == 'e1' or action == 'e8' or action == 'f1' or action == 'f8' or action == 'g1' or action == 'g8' or \
              action == 'h2' or action == 'h3' or action == 'h4' or action == 'h5' or action == 'h6' or action == 'h7':
             opponent_edge_count += 1
+        
+        reverse_count = 0
+        next_state, next_player_id, opponent_reverse_places = get_next_state(state, action, OPPONENT_ID)
+        for opponent_reverse_place in opponent_reverse_places:
+            if opponent_reverse_place == "a2" or opponent_reverse_place == 'a3' or opponent_reverse_place == 'a4' or opponent_reverse_place == 'a5' or opponent_reverse_place == 'a6' or opponent_reverse_place == 'a7' or \
+                 opponent_reverse_place == 'b1' or opponent_reverse_place == 'b8' or opponent_reverse_place == 'c1' or opponent_reverse_place == 'c8' or opponent_reverse_place == 'd1' or opponent_reverse_place == 'd8' or \
+                 opponent_reverse_place == 'e1' or opponent_reverse_place == 'e8' or opponent_reverse_place == 'f1' or opponent_reverse_place == 'f8' or opponent_reverse_place == 'g1' or opponent_reverse_place == 'g8' or \
+                 opponent_reverse_place == 'h2' or opponent_reverse_place == 'h3' or opponent_reverse_place == 'h4' or opponent_reverse_place == 'h5' or opponent_reverse_place == 'h6' or opponent_reverse_place == 'h7':
+                reverse_count += 1
+        
+        if reverse_count > now_reverse_my_edge_count:
+            now_reverse_my_edge_count = reverse_count
 
     for action in actionable:
         # アクションをした時の次の状態を取得
-        next_state, next_player_id = get_next_state(state, action, player_id)
+        next_state, next_player_id, my_reverse_places = get_next_state(state, action, player_id)
+
+        # ひっくり返すことができる相手の端の数
+        reverse_opponent_edge_count = 0
+        for my_reverse_place in my_reverse_places:
+            if my_reverse_place == "a2" or my_reverse_place == 'a3' or my_reverse_place == 'a4' or my_reverse_place == 'a5' or my_reverse_place == 'a6' or my_reverse_place == 'a7' or \
+                 my_reverse_place == 'b1' or my_reverse_place == 'b8' or my_reverse_place == 'c1' or my_reverse_place == 'c8' or my_reverse_place == 'd1' or my_reverse_place == 'd8' or \
+                 my_reverse_place == 'e1' or my_reverse_place == 'e8' or my_reverse_place == 'f1' or my_reverse_place == 'f8' or my_reverse_place == 'g1' or my_reverse_place == 'g8' or \
+                 my_reverse_place == 'h2' or my_reverse_place == 'h3' or my_reverse_place == 'h4' or my_reverse_place == 'h5' or my_reverse_place == 'h6' or my_reverse_place == 'h7':
+                reverse_opponent_edge_count += 1
+                
         # 相手がアクション出来る場所を取得
         next_actionable = get_actionables(next_state, next_player_id)
+
+        # 次の相手がひっくり返すことのできる端の数
+        next_reverse_my_edge_count = 0
+        for next_action in next_actionable:
+            reverse_count = 0
+            next_next_state, next_next_player_id, next_opponent_reverse_places = get_next_state(next_state, next_action, OPPONENT_ID)
+            for next_opponent_reverse_place in next_opponent_reverse_places:
+                if next_opponent_reverse_place == "a2" or next_opponent_reverse_place == 'a3' or next_opponent_reverse_place == 'a4' or next_opponent_reverse_place == 'a5' or next_opponent_reverse_place == 'a6' or next_opponent_reverse_place == 'a7' or \
+                    next_opponent_reverse_place == 'b1' or next_opponent_reverse_place == 'b8' or next_opponent_reverse_place == 'c1' or next_opponent_reverse_place == 'c8' or next_opponent_reverse_place == 'd1' or next_opponent_reverse_place == 'd8' or \
+                    next_opponent_reverse_place == 'e1' or next_opponent_reverse_place == 'e8' or next_opponent_reverse_place == 'f1' or next_opponent_reverse_place == 'f8' or next_opponent_reverse_place == 'g1' or next_opponent_reverse_place == 'g8' or \
+                    next_opponent_reverse_place == 'h2' or next_opponent_reverse_place == 'h3' or next_opponent_reverse_place == 'h4' or next_opponent_reverse_place == 'h5' or next_opponent_reverse_place == 'h6' or next_opponent_reverse_place == 'h7':
+                    reverse_count += 1
+        
+            if reverse_count > next_reverse_my_edge_count:
+                next_reverse_my_edge_count = reverse_count
 
         # 次の相手の角、端を置ける個数を計算
         next_opponent_corner_count = 0
@@ -221,13 +265,15 @@ def choice(actionable, state, player_id):
         evaluate_value += (opponent_corner_count - next_opponent_corner_count) * 10000
         evaluate_value += (opponent_edge_count - next_opponent_edge_count) * 100
         evaluate_value += (100 - len(next_actionable))
-        if next_action == "a1" or action == "a8" or action == "h1" or action == "h8":
+        if action == "a1" or action == "a8" or action == "h1" or action == "h8":
             evaluate_value += 10000
         elif action == "a2" or action == 'a3' or action == 'a4' or action == 'a5' or action == 'a6' or action == 'a7' or \
                 action == 'b1' or action == 'b8' or action == 'c1' or action == 'c8' or action == 'd1' or action == 'd8' or \
                 action == 'e1' or action == 'e8' or action == 'f1' or action == 'f8' or action == 'g1' or action == 'g8' or \
                 action == 'h2' or action == 'h3' or action == 'h4' or action == 'h5' or action == 'h6' or action == 'h7':
             evaluate_value += 100
+        evaluate_value += reverse_opponent_edge_count * 200
+        evaluate_value += (now_reverse_my_edge_count - next_reverse_my_edge_count) * 200
 
         evaluete[action] = evaluate_value
 
@@ -245,7 +291,7 @@ state = ['........', '....01..', '.000000.', '...100..', '..100...', '.....0..',
 actionable = ['d2', 'g4', 'g7', 'd6', 'b2', 'f5', 'h4']
 player_id = "1"
 
-# # next_state, next_player_id = get_next_state(state, "g2", player_id)
+# # next_state, next_player_id, change_places = get_next_state(state, "g2", player_id)
 # # actionable = get_actionables(next_state, next_player_id)
 choice_action = choice(actionable, state, player_id)
 print("choice_action: ", choice_action)
